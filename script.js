c730e907acf70dcd3bf15928103e4ba2b86dcb87
Auriles El Haddad <auriles0404@gmail.com>
2017-02-22 00:03:50 +0100

    Ouverture et fermeture

--- script.js
+++ script.js
@@ -3,22 +3,40 @@ $(document).ready(function(){
 var $mainMenuItems = $("#main-menu ul").children("li");
 var totalMainMenuItems = $mainMenuItems.length;
 var openedIndex = -1;
+
 var init = function () {
   $mainMenuItems.children(".images").click(function()  {
+
     var newIndex = $(this).parent().index();
     var $item = $mainMenuItems.eq(newIndex);
-    var $colorImage = $item.find(".color");
 
-    $colorImage.animate({
-      left: "0px"
-    }, 250);
+    if(openedIndex === newIndex) {
+      animateItem($item, false, 250);
+      openedIndex = -1;
+    } else {
+      if(validIndex(newIndex)) {
+        animateItem($mainMenuItems.eq(openedIndex), false, 250);
+        openedIndex = newIndex;
+        animateItem($item, true, 250);
+      }
+    }
 
-    $item.animate({
-      width: "420px"
-    }, 250);
-
-    openedIndex = newIndex;
   });
+};
+
+var validIndex = function(indexToCheck) {
+  return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems)
+};
+
+var animateItem = function($item, toOpen, speed) {
+
+  var $colorImage = $item.find(".color");
+  var itemParam = toOpen ? {width : "420px"}:{width : "140px"};
+  var colorImageParam = toOpen ? {left : "0px"}:{left : "140px"};
+
+  $colorImage.animate(colorImageParam, speed);
+  $item.animate(itemParam, speed);
+
 };
 
 init();

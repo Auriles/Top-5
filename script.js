297841947335742690ba98f0629c98fae96f768f
Auriles El Haddad <auriles0404@gmail.com>
2017-02-21 23:34:01 +0100

    Animation d'ouverture

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+var $mainMenuItems = $("#main-menu ul").children("li");
+var totalMainMenuItems = $mainMenuItems.length;
+var openedIndex = -1;
+var init = function () {
+  $mainMenuItems.children(".images").click(function()  {
+    var newIndex = $(this).parent().index();
+    var $item = $mainMenuItems.eq(newIndex);
+    var $colorImage = $item.find(".color");
+
+    $colorImage.animate({
+      left: "0px"
+    }, 250);
+
+    $item.animate({
+      width: "420px"
+    }, 250);
+
+    openedIndex = newIndex;
+  });
+};
+
+init();
+
+
+
+
+
+});

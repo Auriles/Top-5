35a1383e0c7b234c3eb4a94f27fca73c4bbceb19
Auriles El Haddad <auriles0404@gmail.com>
2017-02-22 15:04:39 +0100

    Activation des boutons

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
+// Variables
 var $mainMenuItems = $("#main-menu ul").children("li");
 var totalMainMenuItems = $mainMenuItems.length;
 var openedIndex = 2;
 
+// Fonctions
 var init = function () {
   bindEvents();
   if(validIndex(openedIndex)) {
@@ -15,20 +17,22 @@ var bindEvents = function() {
   $mainMenuItems.children(".images").click(function()  {
 
     var newIndex = $(this).parent().index();
-    var $item = $mainMenuItems.eq(newIndex);
-
-    if(openedIndex === newIndex) {
-      animateItem($item, false, 250);
-      openedIndex = -1;
-    } else {
-      if(validIndex(newIndex)) {
-        animateItem($mainMenuItems.eq(openedIndex), false, 250);
-        openedIndex = newIndex;
-        animateItem($item, true, 250);
-      }
-    }
+    checkAndAnimateItem(newIndex);
 
   });
+
+  $(".button").hover(function() {
+    $(this).addClass("hovered");
+  }, function() {
+    $(this).removeClass("hovered");
+  });
+
+  $(".button").click(function() {
+    var newIndex = $(this).index();
+    checkAndAnimateItem(newIndex);
+
+  });
+
 };
 
 var validIndex = function(indexToCheck) {
@@ -46,10 +50,19 @@ var animateItem = function($item, toOpen, speed) {
 
 };
 
+var checkAndAnimateItem = function(indexToCheckAndAnimate) {
+  if(openedIndex === indexToCheckAndAnimate) {
+    animateItem($mainMenuItems.eq(indexToCheckAndAnimate), false, 250);
+    openedIndex = -1;
+  } else {
+    if(validIndex(indexToCheckAndAnimate)) {
+      animateItem($mainMenuItems.eq(openedIndex), false, 250);
+      openedIndex = indexToCheckAndAnimate;
+      animateItem($mainMenuItems.eq(openedIndex), true, 250);
+    }
+  }
+};
+
 init();
-
-
-
-
 
 });
